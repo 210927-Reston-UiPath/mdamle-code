8050b7931e07b99b207269eaa909abbd5393a065
jellyphoto <maldpkdamle@gmail.com>
2021-09-30 12:42:11 -0400

    clean up; started Interface IMenu

--- Week01/Vb/FonBook/Program.vb
+++ Week01/Vb/FonBook/Program.vb
@@ -2,6 +2,32 @@ Imports System
 
 Module Program
     Sub Main(args As String())
+        '-----------------------------------------------------------------------------------------
         Console.WriteLine("Hello World!")
+        Console.WriteLine("BirdS!")
+        Dim num As String = "HI"
+        Dim i As Integer = 0
+        Do While i < 5
+            Console.WriteLine(num)
+            i = i + 1
+        Loop
+
+        '-----------------------------------------------------------------------------------------
+        Dim repeat As Boolean = True
+        Do While repeat
+            Console.WriteLine("Who're you gonna call?")
+            Console.WriteLine("Are you gonna call?")
+            Dim value As Integer = Integer.Parse(Console.ReadLine)
+            Select Case value
+                Case 1
+                    Console.WriteLine("Thank you for choosing to call")
+                Case 2
+                    Console.WriteLine("Goodbye then")
+                    repeat = False 
+                Case Else
+                    Console.WriteLine("what the who?")
+            End Select
+        Loop
+
     End Sub
 End Module
